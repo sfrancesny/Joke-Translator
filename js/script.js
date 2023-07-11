0acb1964f500760652c3d015c979f82e75d3c75e
D. Caine Winters <caine.winters@outlook.com>
2023-07-10 21:27:33 -0400

    Some very perliminary starter code for the starter files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+$(function () {
+    // TODO:  Do all the cool things!
+    console.log("... in the beginning!");
+  });
