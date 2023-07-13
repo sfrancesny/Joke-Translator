da8aa625c8ffce5f3a308c2baefb2f5da3ece411
D. Caine Winters <caine.winters@outlook.com>
2023-07-13 19:34:07 -0400

    Removed eronious directories

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-    // TODO:  Do all the cool things!
-    console.log("... in the beginning!");
-  });
